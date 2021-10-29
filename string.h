fde799aaf908236d9fb8728f170fd21221c4989e
null <60427892+vierofernando@users.noreply.github.com>
2021-10-29 17:02:53 +0700

    Update string.h

--- string.h
+++ string.h
@@ -5,6 +5,6 @@
 
 jsfuck_str_t * jsfuck_str_new(const unsigned short padding);
 void jsfuck_str_append(jsfuck_str_t * ptr, const char * new_str, const unsigned int length);
-char * jsfuck_str_trim(jsfuck_str_t * ptr);
+char * jsfuck_str_trim(jsfuck_str_t * ptr, size_t * s);
 
 #endif
