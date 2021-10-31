aaf24016ba9f57df40163c363dac1e4b09b7fd9c
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-11-01 00:11:35 +0700

    woo

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ JSFuck obfuscator written in ANSI C. For more info/context, visit [www.jsfuck.co
 To build this library, you can clone the repo, compile every C source to an object file, and turn the object files to a static library file or a dynamic library file.
 > Windows users can simply just use the `build.bat` file to build the library.
 
-### Example
+### Library Usage
 ```c
 #include <stdlib.h>
 #include <stdio.h>
@@ -26,3 +26,11 @@ int main() {
 }
 ```
 Pro Tip: You can use `NULL` for the third argument if determining output size is not necessary.
+
+### Compiler
+This library also provides a compiler that compiles a js file to a obfuscated js file. Simply build it (like running `build_compiler.bat` in windows) and do some stuff with the compiler executable!
+
+```
+$ jsfuck test.js
+$ node jsfucked-test.js
+```
--- compiler/compiler.c
+++ compiler/compiler.c
@@ -8,44 +8,44 @@
 #define JSFUCK_FOOTER ")()"
 
 int main(int argc, char ** argv) {
-	if (argc != 2) {
-		puts("error: please add a valid js file to compile.");
-		return 1;
-	}
-	
-	printf("reading %s...", argv[1]);
-	
-	file_contents_t f = read_file(argv[1]);
-	if (f == NULL) {
-		puts("\nerror: file does not exist.");
-		return 1;
-	}
-	
-	printf(" read %d bytes.\ncompiling...", f->file_size);
+    if (argc != 2) {
+        puts("error: please add a valid js file to compile.");
+        return 1;
+    }
+    
+    printf("reading %s...", argv[1]);
+    
+    file_contents_t f = read_file(argv[1]);
+    if (f == NULL) {
+        puts("\nerror: file does not exist.");
+        return 1;
+    }
+    
+    printf(" read %d bytes.\ncompiling...", f->file_size);
 
-	size_t compiled_size;
-	char * compiled = jsfuck(f->contents, f->file_size, &compiled_size);
-	
-	printf(" done.\n");
-	
-	const size_t input_fn_l = strlen(argv[1]);
-	char * o_fn = malloc(input_fn_l + 10);
-	o_fn[input_fn_l + 9] = 0;
-	memcpy(o_fn, "jsfucked-", 9);
-	memcpy(o_fn + 9, argv[1], input_fn_l);
-	
-	FILE * of = fopen(o_fn, "w");
-	
-	fputs(JSFUCK_HEADER, of);
-	fputs(compiled, of);
-	fputs(JSFUCK_FOOTER, of);
-	
-	printf("wrote %d bytes to %s.\n", 776 + compiled_size, o_fn);
-	
-	free_file(f);
-	free(compiled);
-	free(o_fn);
-	fclose(of);
-	
-	return 0;
+    size_t compiled_size;
+    char * compiled = jsfuck(f->contents, f->file_size, &compiled_size);
+    
+    printf(" done.\n");
+    
+    const size_t input_fn_l = strlen(argv[1]);
+    char * o_fn = malloc(input_fn_l + 10);
+    o_fn[input_fn_l + 9] = 0;
+    memcpy(o_fn, "jsfucked-", 9);
+    memcpy(o_fn + 9, argv[1], input_fn_l);
+    
+    FILE * of = fopen(o_fn, "w");
+    
+    fputs(JSFUCK_HEADER, of);
+    fputs(compiled, of);
+    fputs(JSFUCK_FOOTER, of);
+    
+    printf("wrote %d bytes to %s.\n", 776 + compiled_size, o_fn);
+    
+    free_file(f);
+    free(compiled);
+    free(o_fn);
+    fclose(of);
+    
+    return 0;
 }
